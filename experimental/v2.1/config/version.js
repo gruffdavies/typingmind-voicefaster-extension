671cf5a15112ce5564ebd8f80d8641feb32c61af
Gruff Davies <gruff@gruffdavies.com>
2024-11-30 19:40:54 +0000

    tidy (seems to have borked mic canvas?)

--- experimental/v2.1/config/version.js
+++ experimental/v2.1/config/version.js
@@ -1,5 +1,5 @@
 const version = {
-    version: "2.1.26"
+    version: "2.3.4"
   };
 
   document.getElementById("demo-version").textContent = "v" + version.version;
--- experimental/v2.3/config/version.js
+++ experimental/v2.3/config/version.js
@@ -1,5 +1,5 @@
 const version = {
-    version: "2.3.0"
+    version: "2.3.4"
   };
 
   document.getElementById("demo-version").textContent = "v" + version.version;
